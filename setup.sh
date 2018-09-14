72af405944a7eacae53af58720fedbcd0504a775
Nakamura Kosuke <nakamura.k@pepabo.com>
2018-09-14 23:01:24 +0900

    setup.sh

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -x
+
+ln -sf $PWD/.zshrc ~/.zshrc
+ln -sf $PWD/.zsh ~/.zsh
+ln -sf $PWD/.tmux.conf ~/.tmux.conf
+ln -sf $PWD/.vimrc ~/.vimrc
+ln -sf $PWD/.vim ~/.vim
+ln -sf $PWD/.tigrc ~/.tigrc

b245a9b950edd09b46aaac5f526ba61f5b4b08cf
Nakamura Kosuke <nakamura.k@pepabo.com>
2018-09-16 12:31:14 +0900

    update

--- diff-wapper.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/zsh
-
-old=$2
-new=$5
-
-diff -Nau <(lv -c $old) <(lv -c $new) | less
--- setup.sh
+++ setup.sh
@@ -5,5 +5,8 @@ ln -sf $PWD/.zshrc ~/.zshrc
 ln -sf $PWD/.zsh ~/.zsh
 ln -sf $PWD/.tmux.conf ~/.tmux.conf
 ln -sf $PWD/.vimrc ~/.vimrc
-ln -sf $PWD/.vim ~/.vim
 ln -sf $PWD/.tigrc ~/.tigrc
+ln -sf $PWD/.gemrc ~/.gemrc
+ln -sf $PWD/.gitconfig ~/.gitconfig
+ln -sf $PWD/.gitignore_global ~/.gitignore_global
+ln -sf $PWD/.ssh/config ~/.ssh/config
